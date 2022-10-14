7edf846a7d352797447c2c5546d9d616739106ed
xiaoqingyue <31196543+xiaoqingyue@users.noreply.github.com>
2022-10-14 10:17:12 +0800

    Create main.cpp

---
+++ rtsp_sdp/main.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "utils.h"
+#include "rtpenc.h"
+#include "network.h"
+
+int main() {
+
+    int len = 0;
+    int res;
+    uint8_t *stream = NULL;
+    const char *fileName = "/home/zhy/Documents/zzz/MDC-T32/camera/image.h265";
+
+    RTPMuxContext rtpMuxContext;
+    UDPContext udpContext = {
+        .dstIp = "127.0.0.1",   // destination ip
+        .dstPort = 1234         // destination port
+    };
+
+    res = readFile(&stream, &len, fileName);
+    if (res){
+        printf("readFile error.\n");
+        return -1;
+    }
+
+    // create udp socket
+    res = udpInit(&udpContext);
+    if (res){
+        printf("udpInit error.\n");
+        return -1;
+    }
+
+    initRTPMuxContext(&rtpMuxContext);
+    rtpSendH264HEVC(&rtpMuxContext, &udpContext, stream, len);
+
+    free(stream);
+
+    return 0;
+}
